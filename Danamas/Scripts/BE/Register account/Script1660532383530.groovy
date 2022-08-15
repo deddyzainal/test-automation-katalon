e5d7447424930bf73c4743765b759f1730086c83
Meitri Delfiza <meitridelfiza@Meitris-MacBook-Pro.local>
2022-08-15 13:27:07 +0700

    Add case register account using HTTP POST

---
+++ Danamas/Scripts/BE/Register account/Script1660532383530.groovy
@@ -0,0 +1,23 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.util.KeywordUtil
+
+
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+
+//Get new date
+String datetime = new Date().format('yyyyMMddHHmmss')
+
+//Get time
+String time = datetime.drop(8)
+
+//Set request body
+String requestBody = '{"firstname": "Katalon '+datetime+'", "mobile": "+62'+datetime+'", "email": "'+datetime+'@gmail.com", "userType": 1, "adId": null, "password": "Password1", "otpToken": "'+time+'", "referralCode":""}'
+
+//Generate hmac
+String hmac = CustomKeywords.'generateHmac.hmac_sha256'('ksdafkjdsahfuaehfsuhuuuauhfakshdkapqwoe', requestBody)
+
+//Send request
+def response = WS.sendRequest(findTestObject('BE/Register', [('hmac'):hmac, ('datetime'):datetime, ('requestBody'):requestBody]))
+
+//Verify status code 200
+WS.verifyResponseStatusCode(response, 200)
